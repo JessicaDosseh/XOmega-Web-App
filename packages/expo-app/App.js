34aea1d58fdcc8c115eab72a42ed6f1023bc055e
Jessica <Jessica@users-iMac.local>
2020-05-22 20:56:22 -0400

    there seemed to be a problem when the expo node_moduel is gitignored

--- packages/expo-app/App.js
+++ packages/expo-app/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { add } from '@omega/common';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<Text>{add(1, 2)}</Text>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
